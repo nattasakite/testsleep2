332a5354bc2b9d5e5f73a06e9650196994c62721
nattasakite <62010295@kmitl.ac.th>
2021-11-26 11:15:13 +0700

    main

--- config.php
+++ config.php
@@ -1,8 +1,8 @@
 <?php
     // Connect to server and select database.
-    define('servername', 'localhost');
-    define('username', 'root');
-    define('password', '');
-    define('dbname', 'testing');
+    define('servername', 'us-cdbr-east-04.cleardb.com');
+    define('username', 'bee807c0b730a8');
+    define('password', 'f041460d');
+    define('dbname', 'heroku_3d311e173923dfd');
     $objCon = mysqli_connect(servername, username, password,dbname);
 ?>
